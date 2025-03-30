902ec7fe05ac5bb05269df36cd61ac7fe6f75546
FOSSBOSS <blicken@gmail.com>
2025-03-30 16:38:02 -0400

    graceful error msg

--- seesaw/encoder.py
+++ seesaw/encoder.py
@@ -2,23 +2,26 @@
 from smbus2 import SMBus, i2c_msg
 import struct
 import time
-'''
-https://learn.adafruit.com/adafruit-i2c-qt-rotary-encoder/pinouts
-or read Adafruit_seesaw.h from aruino libs
-'''
+import sys
 
-I2C_BUS = 7          # May change from system to system
-I2C_ADDR = 0x36
+I2C_BUS = 4          # Change if needed
+I2C_ADDR = 0x37
 SEESAW_ENCODER_BASE = 0x11
 SEESAW_ENCODER_POSITION = 0x30
 SEESAW_ENCODER_DELTA = 0x40
 
 def read_register(bus, base, reg, length):
-    # Write base + reg
-    write = i2c_msg.write(I2C_ADDR, [base, reg])
-    read = i2c_msg.read(I2C_ADDR, length)
-    bus.i2c_rdwr(write, read)
-    return list(read)
+    try:
+        # Write base + reg
+        write = i2c_msg.write(I2C_ADDR, [base, reg])
+        read = i2c_msg.read(I2C_ADDR, length)
+        bus.i2c_rdwr(write, read)
+        return list(read)
+    except OSError as e:
+        #print(f"I2C Address {I2C_ADDR} not responding.")
+        print(f"I2C Address {I2C_ADDR:02X} not responding.") # crash gracefully
+        print("Check your Bus and address")
+        sys.exit(1)        
 
 def read_encoder_position(bus):
     data = read_register(bus, SEESAW_ENCODER_BASE, SEESAW_ENCODER_POSITION, 4)
